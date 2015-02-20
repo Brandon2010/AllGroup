abd8e4f248afa761d17fd9f6cdbeec15d81d79bd
Shan Gao <Luciferre@Shans-MacBook-Pro.local>
2015-02-20 01:56:20 -0500

    category event activity

---
+++ app/src/main/java/com/cmu/mobilepervasive/allgroup/CategoryActivity.java
@@ -0,0 +1,69 @@
+package com.cmu.mobilepervasive.allgroup;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class CategoryActivity extends ActionBarActivity {
+    private ListView listView;
+    private ArrayList<String> eventList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        listView = (ListView) findViewById(R.id.listView);
+        eventList.add("INI 25th Anniversary");//modify this
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, eventList);
+        listView.setAdapter(myArrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                // TODO Auto-generated method stub
+
+                Intent intent = new Intent(CategoryActivity.this, EventActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_category, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/cmu/mobilepervasive/allgroup/EventActivity.java
@@ -0,0 +1,60 @@
+package com.cmu.mobilepervasive.allgroup;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class EventActivity extends ActionBarActivity {
+    private ListView listView;
+    private ArrayList<String> list = new ArrayList<String>();
+    private TextView description;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        listView = (ListView) findViewById(R.id.listView2);
+        list.add("INI 25th Anniversary");//modify this
+        list.add("Date/Time: ");
+        list.add("Location: ");
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(myArrayAdapter);
+
+
+        description = (TextView)findViewById(R.id.textView);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_event, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
