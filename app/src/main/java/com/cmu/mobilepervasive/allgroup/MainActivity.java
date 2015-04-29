dbe30f69d18b06f4c203ad8f58ec7639e78af968
Nirosun <zuozhengyang@gmail.com>
2015-04-29 13:51:12 -0400

    Merge branches 'master' and 'zz' of github.com:Brandon2010/AllGroup into zz
    # Conflicts:
    #	app/src/main/java/com/cmu/mobilepervasive/allgroup/MainActivity.java

--- app/src/main/java/com/cmu/mobilepervasive/allgroup/MainActivity.java
+++ app/src/main/java/com/cmu/mobilepervasive/allgroup/MainActivity.java
@@ -65,14 +65,12 @@ public class MainActivity extends ActionBarActivity {
     // TODO Use semaphore temporarily
     public static Semaphore semInner = new Semaphore(1, false);
     public static Semaphore semUserCate = new Semaphore(1, false);
-<<<<<<< HEAD
     public static Semaphore semImport = new Semaphore(1, false);
 
 
     public static final int NORMAL = 0, IMPORT = 1;
     public static int state = NORMAL;
-=======
->>>>>>> fd76a0d19f4d2ebe51ae6d4cb4bb52bb0cb1c40b
+
 
     public static long userId = -1;
 
@@ -226,7 +224,7 @@ public class MainActivity extends ActionBarActivity {
             Intent intent = new Intent(MainActivity.this, PrivacyPolicyActivity.class);
             startActivity(intent);
         }
-        else if(item.equals(newCategory)){
+        else if(item.equals(newEvent)){
             SelectionFragment sf = (SelectionFragment)fragments[SELECTION];
             List<Map<String, Object>> filterdata = sf.getFilterData();
             ArrayList<String> list = new ArrayList<>();
@@ -245,7 +243,7 @@ public class MainActivity extends ActionBarActivity {
             intent.putExtra("bundle", bundle);
             startActivity(intent);
         }
-        else if(item.equals(newEvent)){
+        else if(item.equals(newCategory)){
 //            Log.v(TAG, "EDIT");
             AlertDialog.Builder inner = new AlertDialog.Builder(MainActivity.this);
             inner.setTitle(R.string.edit_category);
